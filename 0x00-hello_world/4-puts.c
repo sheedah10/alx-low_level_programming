4548b1f28bc04d486b4e850afd4280316a52851c
sheedah10 <muntalarashida100@gmail.com>
2023-02-16 16:06:06 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include <stdio.h> 
-/*
+#include<stdio.h>
+/**
  * main - print a line of code using puts
  *
- * Return: 0
- */
+ * Return:0
+*/
+
 int main(void)
 {
-	put("/"Programming is like building a multilingual puzzle");
-	return (0):
-}		
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
