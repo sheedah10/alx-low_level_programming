7e494cdd74c597da2b9f2be1394fa1f520d25ec9
sheedah10 <muntalarashida100@gmail.com>
2023-02-16 22:07:29 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 *main -Entry point
 *Return: Always 0 (Success)
 */
-iint main(void)
+int main(void)
 {
         printf("Size of a char: %zu byte(s)\n", sizeof(char));
         printf("Size of an int: %zu byte(s)\n", sizeof(int));
